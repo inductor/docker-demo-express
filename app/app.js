0949ef218b0001693b4a8b418f9162f5d3a321c9
inductor <kohei.ota@zozo.com>
2019-12-30 22:51:03 +0900

    add cors

--- app/app.js
+++ app/app.js
@@ -5,8 +5,11 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3000;
-
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 // GET http://localhost:3000/
 app.get('/', function (req, res) {
@@ -14,6 +17,8 @@ app.get('/', function (req, res) {
         message: "Hello, world!"
     });
 });
+
+const port = process.env.PORT || 3000;
 
 app.listen(port);
 console.log('listen on port ' + port);
